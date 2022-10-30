fe0cd475c7ce2cc4103ed61426d2457fd34658e6
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-30 23:39:23 +0200

    prototype of task3, it's works

---
+++ Sem_1_Lab_1/Task3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+class Card
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the x number: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int xPow = x;
+        Console.WriteLine("Enter the n number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int nCounter = n;
+
+        for (int i = 1; i < nCounter; i++)
+        {
+            x *= xPow;
+
+            if (i == nCounter - 1)
+            {
+                Console.WriteLine("x power x is: " + x);
+            }
+        }
+
+        for (int i = nCounter - 1; i > 0; i--)
+        {
+            n *= i;
+            if (i == 1)
+            {
+                Console.WriteLine("n factorial is: " + n);
+            }
+        }
+    }
+
+}
