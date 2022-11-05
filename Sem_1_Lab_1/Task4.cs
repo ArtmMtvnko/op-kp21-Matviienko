98931fefeff342335f568ff10e5d0815f7db3a6a
Матвієнко Артем <artemm3notegp@gmail.com>
2022-11-05 18:38:27 +0200

    script (plan) of developing task

---
+++ Sem_1_Lab_1/Task4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Pyramid
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a Sinus value (in degrees): ");
+        int degrees = Convert.ToInt32(Console.ReadLine());
+        double x = degrees * (Math.PI / 180);
+        /*
+         * input value (in degrees) -> convert to radians
+         * 
+         * write function for factorial 
+         * 
+         * calculate according formula
+         * 
+         * output the result
+         */
+        Console.WriteLine("Value is: ");
+    }
+
+}
