ba39948c218732211efc117b938c3fb816d522d8
Матвієнко Артем <artemm3notegp@gmail.com>
2022-11-05 19:25:20 +0200

    prototype of program, success test with numbers 1-90

--- Sem_1_Lab_1/Task4.cs
+++ Sem_1_Lab_1/Task4.cs
@@ -5,19 +5,27 @@ class Pyramid
 {
     static void Main(string[] args)
     {
+        static int Fac(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
         Console.WriteLine("Enter a Sinus value (in degrees): ");
         int degrees = Convert.ToInt32(Console.ReadLine());
         double x = degrees * (Math.PI / 180);
-        /*
-         * input value (in degrees) -> convert to radians
-         * 
-         * write function for factorial 
-         * 
-         * calculate according formula
-         * 
-         * output the result
-         */
-        Console.WriteLine("Value is: ");
+
+        double algebraicSum = 0.0;
+        for (int i = 0; i <= 5; i++)
+        {
+            algebraicSum += Math.Pow((-1), i) * (Math.Pow(x, (2 * i + 1))) / Fac(2 * i + 1);
+        }
+        Console.WriteLine("Value is: " + Math.Round(algebraicSum, 3));
+        Console.WriteLine("Control value with defaul library: " + Math.Round(Math.Sin(x), 3));
     }
 
 }
