68c7f951fc7856a7f93a733449ac1a6206982024
Матвієнко Артем <artemm3notegp@gmail.com>
2022-12-10 01:01:19 +0200

    реалізація методів isOpen та isFull

--- sem_1_lab_2/Program.cs
+++ sem_1_lab_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Data.Common;
 using System.Globalization;
 using System.Numerics;
 using System.Xml;
@@ -25,13 +26,21 @@ class Percolate
 
     }
 
-    static bool isOpen(int row, int column)
+    static bool isOpen(int row, int column, int[] array, int n)
     {
+        if (array[(row + n * (row - 1)) + (column - row)] == 1)
+        {
+            return true;
+        }
         return false;
     }
 
-    static bool isFull(int row, int column)
+    static bool isFull(int row, int column, int[] array, int n)
     {
+        if (array[(row + n * (row - 1)) + (column - row)] == 2)
+        {
+            return true;
+        }
         return false;
     }
 
@@ -82,70 +91,97 @@ class Percolate
         int n = Convert.ToInt32(Console.ReadLine());
         int[,] matrix = init(n);
 
-        /*int[,] matrix = new int[n + 1, n + 1];
+        int size = n * n + 2;
+        int[] linerMatrix = new int[size];
 
-        for (int i = 1; i <= n; i++)
+        linerMatrix[0] = 2;
+        linerMatrix[size - 1] = 1;
+        for (int i = 0; i < size; i++)
         {
-            for (int j = 1; j <= n; j++)
-            {
-                matrix[i, j] = 0;
-            }
-        }*/
+            Console.Write(linerMatrix[i] + " ");
+        }
+
+
+        Console.WriteLine();
 
 
         while (true)
         {
-            print(matrix, n);
-
-            Console.WriteLine("Choose the action with matrix: ");
-            Console.WriteLine("1 - open site");
-            Console.WriteLine("2 - check is site open?");
-            Console.WriteLine("3 - check is the site full?");
-            Console.WriteLine("4 - output numbers of open sites");
-            Console.WriteLine("5 - check does the system percolates");
-            Console.WriteLine("6 - exit");
-
-            int num = Convert.ToInt32(Console.ReadLine());
-            if (num < 1 || num > 7)
+            try
             {
-                Console.WriteLine("Error input, enter correctly value 1-6:");
-            }
+                print(matrix, n);
 
-            if (num == 1)
+                Console.WriteLine("Choose the action with matrix: ");
+                Console.WriteLine("1 - open site");
+                Console.WriteLine("2 - check is site open?");
+                Console.WriteLine("3 - check is the site full?");
+                Console.WriteLine("4 - output numbers of open sites");
+                Console.WriteLine("5 - check does the system percolates");
+                Console.WriteLine("6 - exit");
+
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num < 1 || num > 7)
+                {
+                    Console.WriteLine("Error input, enter correctly value 1-6:");
+                }
+
+                if (num == 1)
+                {
+                    Console.WriteLine("Enter number of matrix row and column: ");
+                    Console.Write("Row: ");
+                    byte row = Convert.ToByte(Console.ReadLine());
+                    Console.Write("Column: ");
+                    byte column = Convert.ToByte(Console.ReadLine());
+                    linerMatrix[(row + n * (row - 1)) + (column - row)] = 1;
+                    for (int i = 0; i < size; i++)
+                    {
+                        Console.Write(linerMatrix[i] + " ");
+                    }
+
+                    matrix[row, column] = 1;
+                }
+
+                if (num == 2)
+                {
+                    Console.WriteLine("Enter number of matrix row and column: ");
+                    Console.Write("Row: ");
+                    byte row = Convert.ToByte(Console.ReadLine());
+                    Console.Write("Column: ");
+                    byte column = Convert.ToByte(Console.ReadLine());
+
+                    Console.WriteLine(isOpen(row, column, linerMatrix, n));
+                }
+
+                if (num == 3)
+                {
+                    Console.WriteLine("Enter number of matrix row and column: ");
+                    Console.Write("Row: ");
+                    byte row = Convert.ToByte(Console.ReadLine());
+                    Console.Write("Column: ");
+                    byte column = Convert.ToByte(Console.ReadLine());
+
+                    Console.WriteLine(isFull(row, column, linerMatrix, n));
+                }
+
+                if (num == 4)
+                {
+                    break;
+                }
+
+                if (num == 5)
+                {
+                    break;
+                }
+
+                if (num == 6)
+                {
+                    break;
+                }
+            }
+            catch
             {
-                Console.WriteLine("Enter number of matrix row and column: ");
-                Console.Write("Row: ");
-                byte row = Convert.ToByte(Console.ReadLine());
-                Console.Write("Column: ");
-                byte column = Convert.ToByte(Console.ReadLine());
-                matrix[row, column] = 1;
+                Console.WriteLine("Input Error");
             }
-
-            if (num == 2)
-            {
-                break;
-            }
-
-            if (num == 3)
-            {
-                break;
-            }
-
-            if (num == 4)
-            {
-                break;
-            }
-
-            if (num == 5)
-            {
-                break;
-            }
-
-            if (num == 6)
-            {
-                break;
-            }
-
         }
         Console.WriteLine("end");
     }
