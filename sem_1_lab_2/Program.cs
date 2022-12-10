7d578b054b97fa44e1258c5be0a1f3f613ecd913
Матвієнко Артем <artemm3notegp@gmail.com>
2022-12-11 01:13:45 +0200

    Реалізація методів open(), percolate(), numberOfOpenSites(), isPercolate(), код працює

--- sem_1_lab_2/Program.cs
+++ sem_1_lab_2/Program.cs
@@ -16,13 +16,13 @@ class Percolate
 
     static void union(int x, int y, int[] array)
     {
-        int componentX = array[x];
-        int componentY = array[y];
+        int componentX = array[x]; // -2
+        int componentY = array[y]; // 0
         if (componentX == componentY)
         {
             return;
         }
-        for (int i = 0; i < array.Length; i++)
+        for (int i = 0; i <= array.Length; i++)
         {
             if (array[i] == componentX)
             {
@@ -31,14 +31,50 @@ class Percolate
         }
     }
 
-    static void open(int row, int column)
+    static int find(int x, int[] array)
     {
+        int res = array[x];
+        while (res != array[res])
+        {
+            res = array[res];
+        }
 
+        return res;
+    }
+
+    static void open(int row, int column, int[] array, int n)
+    {
+        array[n * (row - 1) + column] *= (-1);
+        if (column != n)
+        {
+            if (array[n * (row - 1) + column + 1] < 0)
+            {
+                union(n * (row - 1) + column, n * (row - 1) + column + 1, array);
+            }
+        }
+        if (column != 1)
+        {
+            if (array[n * (row - 1) + column - 1] < 0)
+            {
+                union(n * (row - 1) + column, n * (row - 1) + column - 1, array);
+            }
+        }
+        if (row != n)
+        {
+            if (array[n * (row - 1) + column + n] < 0)
+            {
+                union(n * (row - 1) + column, n * (row - 1) + column + n, array);
+            }
+        }
+        if (array[n * (row - 1) + column - n] < 0)
+        {
+            union(n * (row - 1) + column, n * (row - 1) + column - n, array);
+        }
     }
 
     static bool isOpen(int row, int column, int[] array, int n)
     {
-        if (array[n * (row - 1) + column] == 1)
+        if (array[n * (row - 1) + column] < 0)
         {
             return true;
         }
@@ -47,22 +83,49 @@ class Percolate
 
     static bool isFull(int row, int column, int[] array, int n)
     {
-        if (array[n * (row - 1) + column] == 2)
+        if (array[n * (row - 1) + column] == 0)
         {
             return true;
         }
         return false;
     }
 
-    static int numberOfOpenSites()
+    static int numberOfOpenSites(int n, int[] array)
     {
-        return 0;
+        int counter = 0;
+        for (int i = 1; i <= n * n; i++)
+        {
+            if (array[i] <= 0)
+            {
+                counter++;
+            }
+        }
+        return counter;
     }
 
-    static bool percolates()
+    static void percolates(int n, int[] array)
     {
+        for (int i = 1; i <= n; i++)
+        {
+            if (array[i] < 0)
+            {
+                union(i, 0, array);
+            }
+        }
+    }
+
+    static bool isPercolates(int n, int[] array)
+    {
+        for (int i = n * n - n + 1; i <= n * n; i++)
+        {
+            if (array[i] == 0)
+            {
+                return true;
+            }
+        }
         return false;
     }
+
 
     static void print(int n, int[] array)
     {
@@ -70,13 +133,20 @@ class Percolate
         {
             if (array[i] == i)
             {
+                Console.BackgroundColor = ConsoleColor.Red;
                 Console.Write(0 + " ");
             }
-            else
+            if (array[i] < 0)
             {
+                Console.BackgroundColor = ConsoleColor.Green;
                 Console.Write(1 + " ");
             }
-
+            if (array[i] == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(2 + " ");
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
             if (i % n == 0)
             {
                 Console.WriteLine();
@@ -102,33 +172,9 @@ class Percolate
 
         Console.Write("Enter size of matrix: ");
         int n = Convert.ToInt32(Console.ReadLine());
-
-        //int size = n * n + 2;
         int[] linerMatrix = new int[n * n + 2];
 
         init(n, linerMatrix);
-        for (int i = 0; i < n; i++)
-        {
-            union(0, n, linerMatrix);
-        }
-
-        union(5, 10, linerMatrix);
-        union(3, 4, linerMatrix);
-        union(4, 7, linerMatrix);
-        union(11, 14, linerMatrix);
-        union(7, 14, linerMatrix);
-        union(11, 10, linerMatrix);
-        union(8, 15, linerMatrix);
-        union(10, 15, linerMatrix);
-
-        for (int i = 0; i < n; i++)
-        {
-            if (linerMatrix[0] == n * n - i)
-            {
-                union(n * n - i, n * n + 1, linerMatrix);
-            }
-        }
-
 
         Console.WriteLine();
 
@@ -138,15 +184,18 @@ class Percolate
             try
             {
                 print(n, linerMatrix);
-                test(n, linerMatrix);
+                // Console.WriteLine("Linear array: ");
+                // test(n, linerMatrix);
+                Console.WriteLine();
 
                 Console.WriteLine("Choose the action with matrix: ");
                 Console.WriteLine("1 - open site");
                 Console.WriteLine("2 - check is site open?");
                 Console.WriteLine("3 - check is the site full?");
                 Console.WriteLine("4 - output numbers of open sites");
-                Console.WriteLine("5 - check does the system percolates");
-                Console.WriteLine("6 - exit");
+                Console.WriteLine("5 - give a voltage");
+                Console.WriteLine("6 - check does the system percolates");
+                Console.WriteLine("7 - exit");
 
                 int num = Convert.ToInt32(Console.ReadLine());
                 if (num < 1 || num > 7)
@@ -161,7 +210,8 @@ class Percolate
                     byte row = Convert.ToByte(Console.ReadLine());
                     Console.Write("Column: ");
                     byte column = Convert.ToByte(Console.ReadLine());
-                    linerMatrix[n * (row - 1) + column] = 1;
+
+                    open(row, column, linerMatrix, n);
                 }
 
                 if (num == 2)
@@ -172,7 +222,7 @@ class Percolate
                     Console.Write("Column: ");
                     byte column = Convert.ToByte(Console.ReadLine());
 
-                    Console.WriteLine(isOpen(row, column, linerMatrix, n));
+                    Console.WriteLine("Is the site open?: " + isOpen(row, column, linerMatrix, n));
                 }
 
                 if (num == 3)
@@ -183,20 +233,25 @@ class Percolate
                     Console.Write("Column: ");
                     byte column = Convert.ToByte(Console.ReadLine());
 
-                    Console.WriteLine(isFull(row, column, linerMatrix, n));
+                    Console.WriteLine("Is the site full?: " + isFull(row, column, linerMatrix, n));
                 }
 
                 if (num == 4)
                 {
-                    break;
+                    Console.WriteLine("Number of open sites = " + numberOfOpenSites(n, linerMatrix));
                 }
 
                 if (num == 5)
                 {
-                    break;
+                    percolates(n, linerMatrix);
                 }
 
                 if (num == 6)
+                {
+                    Console.WriteLine("Does system percolates?: " + isPercolates(n, linerMatrix));
+                }
+
+                if (num == 7)
                 {
                     break;
                 }
