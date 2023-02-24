7f6f77fe2b0909510f3fa3607115932eb2bf1749
Матвієнко Артем <artemm3notegp@gmail.com>
2023-02-24 23:47:43 +0200

    Додання шаблону та сценарію до Task2

--- Sem_2_lab_1/Task2/Program.cs
+++ Sem_2_lab_1/Task2/Program.cs
@@ -7,41 +7,24 @@ class Class1
 {
     static void Main(string[] args)
     {
-        // 1) Create new file .txt
-        // 2) Write there 2 line of text
-        // 3) Read this file
-        // 4) Output result in console
+        // Ganarate random numbers
+        // Create txt file
+        // Write all numbers in file
+        // Create another file
+        // Write the max value of numbers in new file
 
         string line;
         try
         {
-            StreamWriter sw = new StreamWriter(@"D:\Git\Progects\Sem_2_lab_1\Task1\Text1.txt");
-
-            sw.WriteLine("Hello World!");
-            sw.WriteLine("Test Lab 1.");
-
-            sw.Close();
-
-            StreamReader sr = new StreamReader(@"D:\Microsoft Visual Studio\Projects\OP_Sem_2_lab_1\OP_Sem_2_lab_1\Text1.txt");
-
-            line = sr.ReadLine();
-
-            while (line != null)
-            {
-                Console.WriteLine(line);
-
-                line = sr.ReadLine();
-            }
-
-            sr.Close();
+            
         }
-        catch
+        catch (Exception e)
         {
-
+            Console.WriteLine("Exception: " + e.Message);
         }
         finally
         {
-
+            Console.WriteLine("Executing finally block");
         }
     }
 
