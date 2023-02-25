f6389ce018b6c2626cfada45eac8a7d4963013ef
Матвієнко Артем <artemm3notegp@gmail.com>
2023-02-25 18:04:03 +0200

    Створення шаблону та сценарію до Task3

---
+++ Sem_2_lab_1/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+        // Read the lines
+        // Save strings in array
+        // Sort array
+        // Write words in new file
+
+        string line;
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block");
+        }
+    }
+
+}
