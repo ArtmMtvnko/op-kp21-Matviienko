5626756848844eb2fc0a314eb1550ea25451425e
Матвієнко Артем <artemm3notegp@gmail.com>
2023-02-25 23:44:10 +0200

    Було додано шаблон та сценарій програми

---
+++ Sem_2_lab_1/Task5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+class Class1
+{
+    static void Main(string[] args)
+    {
+        // Use only arrays
+        // Read the file
+        // Save in odd indexes word, in even indexes - amount
+        // Output result
+
+        string line;
+        try
+        {
+            StreamReader sr = new StreamReader(@"D:\Git\Progects\Sem_2_lab_1\Task4\Text4.txt\Text4.csv\Text5.csv");
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block");
+        }
+    }
+
+}
