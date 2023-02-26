330edcae3da3d06cc020a182a730dd8feebf4433
Матвієнко Артем <artemm3notegp@gmail.com>
2023-02-26 17:03:40 +0200

    Додання 'шаблону' та сценарію до розробки

---
+++ Sem_2_lab_1/Task6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+class Class1
+{
+    
+    static void Main(string[] args)
+    {
+        // Create binary file that copy content of .csv file
+        // Read the binary file
+        // Sorted stutents with score > 95
+        // Write sorted students and theirs amount in new binary file
+
+        string line;
+        try
+        {
+            StreamReader sr = new StreamReader(@"D:\Git\Progects\Sem_2_lab_1\Task4\Text4.txt\Text4.csv\Text5.txt");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block");
+        }
+    }
+
+}
