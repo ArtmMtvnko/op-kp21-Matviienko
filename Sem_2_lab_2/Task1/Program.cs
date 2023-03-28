d82323821241dd56fb35d5a7646a15d78b02b3f5
Матвієнко Артем <artemm3notegp@gmail.com>
2023-03-28 18:55:59 +0300

    'Заглушки' для 1 завдання

---
+++ Sem_2_lab_2/Task1/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text;
+
+namespace OP_Sem_2_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  
+             *  Створити класс для виведення інтерфейту у консоль
+             *  Записати результати таблиці у .csv файл
+             *  Надати доступ до таблиці через .csv файл
+             *  
+             */
+
+
+        }
+    }
+}
