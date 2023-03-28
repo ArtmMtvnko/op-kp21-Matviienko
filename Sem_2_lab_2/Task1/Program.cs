3bb87615d27aaf43ce89bda2794ceeac84691b65
Матвієнко Артем <artemm3notegp@gmail.com>
2023-03-28 18:59:50 +0300

    Прототип до классу для працювання з інтерфейсом у консолі

--- Sem_2_lab_2/Task1/Program.cs
+++ Sem_2_lab_2/Task1/Program.cs
@@ -14,7 +14,50 @@ namespace OP_Sem_2_Lab_2
              *  
              */
 
+            // Для коректного відображення текста Кирилицею
+            Console.OutputEncoding = Encoding.UTF8;
 
+            ConsoleInterface menu = new ConsoleInterface();
+
+            menu.PrintMenuItem("1. Додати людину");
+            menu.PrintMenuItem("2. Видалити людину");
+            menu.PrintMenuItem("3. Змінити параметри");
+
+            menu.CheckInput();
+
+            Console.WriteLine("out of range");
+        }
+    }
+
+    class ConsoleInterface
+    {
+        private int _menuLength = 0;
+
+        public void PrintMenuItem(string str)
+        {
+            Console.WriteLine(str);
+            _menuLength++;
+        }
+
+        public void CheckInput()
+        {
+            while (true)
+            {
+                try
+                {
+                    int num = Int32.Parse(Console.ReadLine());
+                    if (num < 1 || num > _menuLength)
+                    {
+                        Console.WriteLine($"Error input, enter correctly value 1-{_menuLength}:");
+                        continue;
+                    }
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
         }
     }
 }
