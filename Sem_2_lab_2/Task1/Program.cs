94fcdd350575f2164837900fb801cc13c281ea43
Матвієнко Артем <artemm3notegp@gmail.com>
2023-04-07 18:51:02 +0300

    повністю змінено логіку, видалено старі малоефективні класи, код працює

--- Sem_2_lab_2/Task1/Program.cs
+++ Sem_2_lab_2/Task1/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using System.IO;
 using System.ComponentModel.Design;
+using System.Linq;
 
 namespace OP_Sem_2_Lab_2
 {
@@ -9,21 +10,6 @@ namespace OP_Sem_2_Lab_2
     {
         static void Main(string[] args)
         {
-            Person a = new Person("loh", 100, 30);
-            Person b = new Person("Ivan", 1050, 10);
-            Person c = new Person("Andrei", 1900, 1000);
-
-            Editor editor = new Editor();
-            editor.AddItem(a);
-            editor.AddItem(b);
-            editor.AddItem(c);
-            editor.AddItem("Molli", 0, 0);
-
-            editor.ShowItems();
-
-            editor.DeleteItem("loh");
-            editor.ShowItems();
-
             /*  
              *  Створити класс для виведення інтерфейту у консоль
              *  Записати результати таблиці у .csv файл
@@ -34,6 +20,9 @@ namespace OP_Sem_2_Lab_2
             // Для коректного відображення текста Кирилицею
             Console.OutputEncoding = Encoding.UTF8;
 
+            Editor editor = new Editor(@"D:\Microsoft Visual Studio\Projects\OP_Sem_2_Lab_2\OP_Sem_2_Lab_2\table.csv");
+            editor.PullChanges();
+
             ConsoleInterface mainMenu = new ConsoleInterface();
 
             while (true)
@@ -41,7 +30,9 @@ namespace OP_Sem_2_Lab_2
                 mainMenu.MenuLength = 0;
                 mainMenu.PrintMenuItem("1. Додати людину");
                 mainMenu.PrintMenuItem("2. Видалити людину");
-                mainMenu.PrintMenuItem("3. Записати зміни");
+                mainMenu.PrintMenuItem("3. Зберегти зміни");
+
+                editor.ShowItems();
 
                 int menuNumber = mainMenu.GetCheckedInput();
 
@@ -50,28 +41,31 @@ namespace OP_Sem_2_Lab_2
                     case 1:
                         Console.Clear();
                         Console.WriteLine("Вкажіть прізвище");
-                        // видаляємо зайві пробіли
-                        string name = Console.ReadLine().Replace(" ", "");
+                        string name = Console.ReadLine().Replace(" ", ""); // видаляємо зайві пробіли
 
-                        WriterName lastName = new WriterName(@"D:\Microsoft Visual Studio\Projects\OP_Sem_2_Lab_2\OP_Sem_2_Lab_2\table.csv");
-                        lastName.Input(name);
+                        Console.WriteLine("Вкажіть заробітню платню");
+                        int salary = Int32.Parse(Console.ReadLine().Replace(" ", ""));
+
+                        Console.WriteLine("Вкажіть утриману з/п");
+                        int holdedSalary = Int32.Parse(Console.ReadLine().Replace(" ", ""));
+
+                        editor.AddItem(new Person(name, salary, holdedSalary));
 
                         Console.Clear();
                         break;
                     case 2:
                         Console.Clear();
+
                         Console.WriteLine("Вкажіть рядок, який ви бажаєте видалити");
+                        int deleteNumber = Int32.Parse(Console.ReadLine().Replace(" ", ""));
 
-                        DeleteLine deleteLine = new DeleteLine(@"D:\Microsoft Visual Studio\Projects\OP_Sem_2_Lab_2\OP_Sem_2_Lab_2\table.csv");
-
-                        int deleteLineNumber = deleteLine.GetLineNumber();
-                        deleteLine.Delete(deleteLineNumber);
+                        editor.DeleteItem(deleteNumber);
 
                         Console.Clear();
                         break;
                     case 3:
-                        break;
-                    default:
+                        Console.Clear();
+                        Console.WriteLine("У цьому немає потреби, файли автоматично зберігаються у файл ;)");
                         break;
                 }
 
@@ -105,21 +99,41 @@ namespace OP_Sem_2_Lab_2
 
     class Editor
     {
+        private string _path;
         protected List<string> lines = new List<string>();
+
+        public Editor(string path)
+        {
+            _path = path;
+        }
+
+        private void WriteChangesToFile()
+        {
+            using (StreamWriter sw = new StreamWriter(_path, false))
+            {
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    sw.WriteLine($"{i + 1},{lines[i]}");
+                }
+            }
+        }
 
         public void AddItem(Person person)
         {
             lines.Add(person.ToString());
+            WriteChangesToFile();
         }
 
         public void AddItem(string name = "undefined", int salary = 0, int holdedSalary = 0)
         {
             lines.Add(new Person(name, salary, holdedSalary).ToString());
+            WriteChangesToFile();
         }
 
         public void DeleteItem(int numberOfLine)
         {
             lines.RemoveAt(--numberOfLine);
+            WriteChangesToFile();
         }
 
         public void DeleteItem(string name)
@@ -130,14 +144,31 @@ namespace OP_Sem_2_Lab_2
                 lines.Remove(line);
                 break;
             }
+            WriteChangesToFile();
+        }
+
+        public void PullChanges()
+        {
+            using (StreamReader sr = new StreamReader(_path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    string result = string.Join(',', parts.Skip(1)); // String.Join(); s(S)
+                    lines.Add(result);
+                }
+            }
         }
 
         public void ShowItems()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             for (int i = 0; i < lines.Count; i++)
             {
                 Console.WriteLine($"{i + 1},{lines[i]}");
             }
+            Console.ResetColor();
         }
 
 
@@ -181,111 +212,4 @@ namespace OP_Sem_2_Lab_2
         }
     }
 
-    abstract class Writer
-    {
-        protected string _path;
-        public abstract void Input(string massage);
-    }
-
-    class WriterName : Writer
-    {
-        public WriterName(string path)
-        {
-            _path = path;
-        }
-
-        // мб видалити проперті
-        public string Path
-        {
-            get { return _path; }
-            set { _path = value; }
-        }
-
-        public override void Input(string massage)
-        {
-            // true - означає, що ми не перезаписуємо файл, а відкриваємо і записуємо у кінець файла
-            using (StreamWriter sw = new StreamWriter(_path, true))
-            {
-                sw.WriteLine(massage);
-                sw.Close(); // Flush()
-            }
-        }
-    }
-
-    abstract class Deleter
-    {
-        protected string _path;
-        public abstract void Delete(int NumberOfline);
-    }
-
-    class DeleteLine : Deleter
-    {
-        public DeleteLine(string path)
-        {
-            _path = path;
-        }
-
-        public override void Delete(int NumberOfLine)
-        {
-            List<string> lines = new List<string>();
-
-            // true - означає, що ми не перезаписуємо файл, а відкриваємо і записуємо у кінець файла
-            using (StreamReader sr = new StreamReader(_path, true))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    lines.Add(line);
-                }
-
-                sr.Close(); // Flush()
-            }
-
-            lines.RemoveAt(NumberOfLine - 1);
-
-            using (StreamWriter sw = new StreamWriter(_path))
-            {
-                foreach (string line in lines)
-                {
-                    sw.WriteLine(line);
-                }
-
-                sw.Close();
-            }
-        }
-
-        public int GetLineNumber()
-        {
-            int tableLength = 0;
-            using (StreamReader sr = new StreamReader(_path))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    tableLength++;
-                }
-            }
-            int deleteLineNumber;
-            while (true)
-            {
-                try
-                {
-                    deleteLineNumber = Int32.Parse(Console.ReadLine().Replace(" ", ""));
-                    if (deleteLineNumber > tableLength || deleteLineNumber < 1)
-                    {
-                        Console.WriteLine($"Error input, enter correctly value 1-{tableLength}:");
-                        continue;
-                    }
-                    break;
-                }
-                catch
-                {
-                    Console.WriteLine($"Error input");
-                }
-            }
-
-            return deleteLineNumber;
-
-        }
-    }
 }
