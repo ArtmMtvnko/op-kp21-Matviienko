e163cfa832cfb050b6e5bcca0b17a098237c3fb7
Матвієнко Артем <artemm3notegp@gmail.com>
2023-04-10 22:03:57 +0300

    Додано невеликий консольний інтерфейс який оброблює ввід даних про координати двох векторів

--- Sem_2_lab_2/Task3/Program.cs
+++ Sem_2_lab_2/Task3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Numerics;
+using System.Text;
 
 namespace Task3
 {
@@ -7,9 +8,37 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            Vector a = new Vector(1,1,2,3,4,1,1,2);
+            Console.OutputEncoding = Encoding.UTF8;
 
-            Console.WriteLine((-a).ToString());
+            int[] aa = { 1, 2, 3, 4, 4, 0 };
+            Vector a;
+            Vector b;
+
+            ConsoleInterface menu = new ConsoleInterface();
+            menu.Reset();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(menu.SetMenuItem("Вкажіть координати Vector 1 через кому:"));
+
+                string[] inputVector1 = Console.ReadLine().Replace(" ", "").Split(',');
+                if (!menu.CheckInput(inputVector1)) continue;
+                int[] arrayOfNumbersVector1 = Array.ConvertAll(inputVector1, Int32.Parse);
+
+                a = new Vector(arrayOfNumbersVector1);
+
+                Console.Clear();
+                Console.WriteLine(menu.SetMenuItem("Вкажіть координати Vector 2 через кому:"));
+
+                string[] inputVector2 = Console.ReadLine().Replace(" ", "").Split(',');
+                if (!menu.CheckInput(inputVector2)) continue;
+                int[] arrayOfNumbersVector2 = Array.ConvertAll(inputVector2, Int32.Parse);
+
+                b = new Vector(arrayOfNumbersVector2);
+
+                break;
+            }
         }
     }
 
@@ -21,38 +50,56 @@ namespace Task3
             _coordinates = numbers;
         }
 
-        public static Vector operator -(Vector vector)
+        public static int operator +(Vector a, Vector b)
         {
-            int[] numbers = new int[vector._coordinates.Length];
+            int summaryNumbers = 0;
 
-            for (int i = 0; i < vector._coordinates.Length; i++)
+            for (int i = 0; i < a._coordinates.Length; i++)
             {
-                numbers[i] = (-vector._coordinates[i]);
+                if (a._coordinates[i] < 0) summaryNumbers += a._coordinates[i];
             }
 
-            return new Vector(numbers);
-        }
-        /*
-        public static Vector operator +(Vector a, Vector b)
-        {
-            return new Vector(a.X + b.X, a.Y + b.Y);
-        }
+            for (int i = 0; i < b._coordinates.Length; i++)
+            {
+                if (b._coordinates[i] < 0) summaryNumbers += a._coordinates[i];
+            }
 
-        public static Vector operator -(Vector a, Vector b)
-        {
-            return a + (-b);
+            return summaryNumbers;
         }
 
         public static int operator *(Vector a, Vector b)
         {
-            return ( a.X * b.X ) + ( a.Y * b.Y );
+            int multiplyNumbers = 1;
+
+            for (int i = 1; i < a._coordinates.Length; i += 2)
+            {
+                multiplyNumbers *= a._coordinates[i];
+            }
+
+            for (int i = 1; i < b._coordinates.Length; i += 2)
+            {
+                multiplyNumbers *= b._coordinates[i];
+            }
+
+            return multiplyNumbers;
         }
 
         public static int operator /(Vector a, Vector b)
         {
-            return (a.X * b.X) + (a.Y * b.Y);
+            int zeroCounter = 0;
+
+            for (int i = 0; i < a._coordinates.Length; i++)
+            {
+                if (a._coordinates[i] == 0) zeroCounter++;
+            }
+
+            for (int i = 0; i < b._coordinates.Length; i++)
+            {
+                if (b._coordinates[i] == 0) zeroCounter++;
+            }
+
+            return zeroCounter;
         }
-        */
 
         public override string ToString()
         {
@@ -64,6 +111,46 @@ namespace Task3
             }
 
             return String.Format("Vector: ( {0} )", output);
+        }
+    }
+
+    class ConsoleInterface
+    {
+        private int _menuLength;
+
+        public ConsoleInterface()
+        {
+            _menuLength = 0;
+        }
+
+        public void Reset()
+        {
+            _menuLength = 0;
+        }
+
+        public string SetMenuItem(string message)
+        {
+            _menuLength++;
+            return message;
+        }
+
+        public bool CheckInput(string[] values)
+        {
+            int number;
+            foreach (string value in values)
+            {
+                bool check = int.TryParse(value, out number);
+                if (!check) return false;
+            }
+            return true;
+        }
+
+        public void ShowArray(int[] array)
+        {
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
         }
     }
 }
